6ec2ff841438df3bc5a65ffde6a84d4eac9d9a4b
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-21 10:26:51 -0700

    Set initial values and usestates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Route, Link, Switch } from 'react-router-dom'
 
 import Home from './components/Home'
+import PizzaForm from './components/PizzaForm'
+
+const initialOrders = []
+
+const initialFormValues = {
+    name: '',
+    size: '',
+    pepperoni: false,
+    sausage: false,
+    fetaCheese: false,
+    onions: false,
+    choppedGarlic: false,
+    roastedRedPeppers: false,
+    special: ''
+}
+
+const initialFormErrors = {
+    name: '',
+    size: '',
+    special: ''
+}
+
+const initialDisabled = true
 
 const App = () => {
+
+    const [orders, setOrders] = useState(initialOrders)
+    const [formValues, setFormValues] = useState(initialFormValues)
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
+    const [disabled, setDisabled] = useState(initialDisabled)
+
+    const postNewOrder = newOrder => {
+        axios.post('https://reqres.in/api/orders', newOrder)
+            .then(res => {
+                setOrders([res.data, ...orders])
+            })
+            .catch(err => {
+                console.log(err)
+            })
+            .finally(() => {
+                setFormValues(initialFormValues)
+            })
+    }
+
   return (
     <div className='app'>
         <nav>
@@ -13,6 +56,7 @@ const App = () => {
 
         <Switch>
             <Route path='/' component={Home} />
+            <Route path='/pizza' component={PizzaForm}/>
         </Switch>
     </div>
   );
