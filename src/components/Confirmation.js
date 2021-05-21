344d33ea43320a38636de9b31ebfea5c9c367e48
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-21 16:06:18 -0700

    Add styling to order form and confirmation page

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -3,7 +3,11 @@ import React from 'react'
 function Confirmation() {
     return (
         <div className='confirmation'>
-            <h2>Congratulations! Your order has been place!</h2>
+            <h2>Congratulations! Your order has been placed and is on it's way!</h2>
+            <img
+            src='https://static.vecteezy.com/system/resources/thumbnails/001/810/791/small_2x/motorcycle-pizza-delivery-courier-with-face-mask-free-vector.jpg'
+            alt='Illustration of man delivering pizza on scooter'
+            />
         </div>
     )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -19,97 +19,99 @@ function Form(props) {
 
     return (
         <form className='container' id='pizza-form' onSubmit={onSubmit}>
-            <label>Name
-                <input
-                id='name-input'
-                type='text'
-                name='name'
-                value={values.name}
-                onChange={onChange}
-                />
-            </label>
+            <div className='inputs'>
+                <label>Name
+                    <input
+                    id='name-input'
+                    type='text'
+                    name='name'
+                    value={values.name}
+                    onChange={onChange}
+                    />
+                </label>
 
-            <label>Size
-                <select
-                id='size-dropdown'
-                name='size'
-                value={values.size}
-                onChange={onChange}
-                >
-                    <option value=''>-- Select a size --</option>
-                    <option value='small'>Small</option>
-                    <option value='medium'>Medium</option>
-                    <option value='large'>Large</option>
-                </select>
-            </label>
+                <label>Size
+                    <select
+                    id='size-dropdown'
+                    name='size'
+                    value={values.size}
+                    onChange={onChange}
+                    >
+                        <option value=''>-- Select a size --</option>
+                        <option value='small'>Small</option>
+                        <option value='medium'>Medium</option>
+                        <option value='large'>Large</option>
+                    </select>
+                </label>
 
-            <div className='toppings'>
                 <h3>Toppings</h3>
-                <label>Pepperoni
-                    <input
-                    type='checkbox'
-                    name='pepperoni'
-                    checked={values.pepperoni}
-                    onChange={onChange}
-                    />
-                </label>
+                <div className='toppings'>
+                    
+                    <label>Pepperoni
+                        <input
+                        type='checkbox'
+                        name='pepperoni'
+                        checked={values.pepperoni}
+                        onChange={onChange}
+                        />
+                    </label>
 
-                <label>Sausage
-                    <input
-                    type='checkbox'
-                    name='sausage'
-                    checked={values.sausage}
-                    onChange={onChange}
-                    />
-                </label>
+                    <label>Sausage
+                        <input
+                        type='checkbox'
+                        name='sausage'
+                        checked={values.sausage}
+                        onChange={onChange}
+                        />
+                    </label>
 
-                <label>Feta Cheese
-                    <input
-                    type='checkbox'
-                    name='fetaCheese'
-                    checked={values.fetaCheese}
-                    onChange={onChange}
-                    />
-                </label>
+                    <label>Feta Cheese
+                        <input
+                        type='checkbox'
+                        name='fetaCheese'
+                        checked={values.fetaCheese}
+                        onChange={onChange}
+                        />
+                    </label>
 
-                <label>Onions
-                    <input
-                    type='checkbox'
-                    name='onions'
-                    checked={values.onions}
-                    onChange={onChange}
-                    />
-                </label>
+                    <label>Onions
+                        <input
+                        type='checkbox'
+                        name='onions'
+                        checked={values.onions}
+                        onChange={onChange}
+                        />
+                    </label>
 
-                <label>Chopped Garlic
-                    <input
-                    type='checkbox'
-                    name='choppedGarlic'
-                    checked={values.choppedGarlic}
-                    onChange={onChange}
-                    />
-                </label>
+                    <label>Chopped Garlic
+                        <input
+                        type='checkbox'
+                        name='choppedGarlic'
+                        checked={values.choppedGarlic}
+                        onChange={onChange}
+                        />
+                    </label>
 
-                <label>Roasted Red Peppers
+                    <label>Roasted Red Peppers
+                        <input
+                        type='checkbox'
+                        name='roastedRedPeppers'
+                        checked={values.roastedRedPeppers}
+                        onChange={onChange}
+                        />
+                    </label>
+                </div>
+
+                <label>Special Instructions
                     <input
-                    type='checkbox'
-                    name='roastedRedPeppers'
-                    checked={values.roastedRedPeppers}
+                    id='special-text'
+                    type='text'
+                    name='special'
+                    value={values.special}
                     onChange={onChange}
                     />
                 </label>
             </div>
-
-            <label>Special Instructions
-                <input
-                id='special-text'
-                type='text'
-                name='special'
-                value={values.special}
-                onChange={onChange}
-                />
-            </label>
-
             <div className='submit'>
                 <button disabled={disabled} id='order-button'>Place Order</button>
 
